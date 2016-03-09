5610f350abe2c9f68870f67e4eb12148bb4c0bf5
felipeprado <felipeprado@ICCSW-026189.local.inatel.br>
2016-03-09 13:32:33 -0300

    Prototype prototype.java.jsf.project2
    - Configuration of JPA with Hibernate.

--- java/prototype.java.jsf.project2/src/main/java/prototype/java/jsf/project2/controllers/UserController.java
+++ java/prototype.java.jsf.project2/src/main/java/prototype/java/jsf/project2/controllers/UserController.java
@@ -8,7 +8,6 @@ import javax.faces.view.ViewScoped;
 import javax.inject.Named;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
-import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
 import prototype.java.jsf.project2.models.PeopleModel;
 import prototype.java.jsf.project2.models.UserModel;
