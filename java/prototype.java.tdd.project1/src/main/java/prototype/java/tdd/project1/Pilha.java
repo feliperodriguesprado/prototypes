aafb4f536343109b38570b12d652f1aadb65e15c
felipeprado <felipeprado@Aspire-5750>
2016-06-01 20:00:10 -0300

    Add prototype TDD with test unit.

---
+++ java/prototype.java.tdd.project1/src/main/java/prototype/java/tdd/project1/Pilha.java
@@ -0,0 +1,13 @@
+package prototype.java.tdd.project1;
+
+class Pilha {
+
+    public boolean estaVazia() {
+        return true;
+    }
+
+    public int tamaho() {
+        return 0;
+    }
+
+}
---
+++ java/prototype.java.tdd.project1/src/test/java/prototype/java/tdd/project1/TestePilha.java
@@ -0,0 +1,37 @@
+package prototype.java.tdd.project1;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class TestePilha {
+
+    public TestePilha() {
+    }
+
+    @BeforeClass
+    public static void setUpClass() {
+    }
+
+    @AfterClass
+    public static void tearDownClass() {
+    }
+
+    @Before
+    public void setUp() {
+    }
+
+    @After
+    public void tearDown() {
+    }
+
+    @Test
+    public void hello() {
+        Pilha p = new Pilha();
+        assertTrue(p.estaVazia());
+        assertEquals(0, p.tamaho());
+    }
+}
